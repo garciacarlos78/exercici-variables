3a4bfa7a480784a322a85f18a07bb09dc1384b32
garciacarlos78 <51118101+garciacarlos78@users.noreply.github.com>
2020-02-18 13:41:20 +0100

    Added Fase4

--- src/exercici_variables/Fase4.java
+++ src/exercici_variables/Fase4.java
@@ -1,6 +1,6 @@
 package exercici_variables;
 
-public class Fase3 {
+public class Fase4 {
 	
 	public static void main(String[] args) {
 		
@@ -8,14 +8,18 @@ public class Fase3 {
 		String nom = "Carlos";
 		String cognom1 = "Garcia";
 		String cognom2 = "Romero";
-		String leapBirth = "El teu any de neixement és de traspàs.";
-		String notLeapBirth = "El teu any de neixement no és de traspàs.";
-
+		String leapBirth = "El meu any de neixement és de traspàs.";
+		String notLeapBirth = "El meu any de neixement no és de traspàs.";
+		
 		// int vars
 		int dia = 27;
 		int mes = 12;
 		int any = 1978;
 		int base_year = 1948;
+
+		// Fase 4
+		String fullName = nom + " " + cognom1 + " " + cognom2;
+		String birthDate = dia + "/" + mes + "/" + any;
 	
 		// Cada quan hi ha un any de traspàs
 		int leap = 4;
@@ -23,14 +27,18 @@ public class Fase3 {
 		// Anys de traspàs entre 1948 i any de naixement
 		int num_leap = (any - base_year) / leap;
 		
-		// Si queremos incluir 1948, eliminar 'base_year+=4'
+		// Si volem incloure 1948, eliminar 'base_year+=4'
 		for (base_year+=4; base_year<=any; base_year+=4) 
 		{
-			System.out.println(base_year);						
+			// System.out.println(base_year);						
 		}
 		
 		// Si l'any de naixement és de traspàs base_year tindrà el valor del següent any de traspàs
 		boolean traspas=(base_year-4 == any);
+		
+		// Fase 4
+		System.out.println("El meu nom és " + fullName);
+		System.out.println("Vaig nèixer el " + birthDate);
 		
 		if (traspas) {
 			System.out.println(leapBirth);
