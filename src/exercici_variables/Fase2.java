b96633b1ef2e9c86c525198db634c6bfb159df8e
garciacarlos78 <51118101+garciacarlos78@users.noreply.github.com>
2020-02-18 13:01:42 +0100

    Added Fase1, Fase2

---
+++ src/exercici_variables/Fase2.java
@@ -0,0 +1,31 @@
+package exercici_variables;
+
+public class Fase2 {
+	
+	// Constant declaration
+	static final int BASE_YEAR = 1948; 
+
+	public static void main(String[] args) {
+		
+		// String vars
+		String nom = "Carlos";
+		String cognom1 = "Garcia";
+		String cognom2 = "Romero";
+		
+		// int vars
+		int dia = 27;
+		int mes = 12;
+		int any = 1948;
+	
+		// Cada quan hi ha un any de traspàs
+		int leap = 4;
+		
+		// Anys de traspàs entre 1948 i any de naixement
+		int num_leap = (any - BASE_YEAR) / 2;
+		
+		// Mostrar per pantalla
+		// 1948 no incluido. Para incluirlo, ++num_leap en el println
+		System.out.println(num_leap);		
+	}
+
+}
